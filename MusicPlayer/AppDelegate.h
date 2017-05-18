f3c1a9f5bd4e4bf41e2b6914365fa145c58c0170
IGPL <igpl0016@IGPLs-Mac-mini.local>
2017-05-18 17:47:21 +0530

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by IGPL on 18/05/17.
+//  Copyright © 2017 IGPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
